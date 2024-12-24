9f41caf8da7cad5ab301d09d5b7269fafc9313f3
Erzis900 <erzis900@gmail.com>
2024-12-24 22:50:48 +0100

    collision checking (proj->enemy)

--- src/features/projectile/systems/checkCollision.cpp
+++ src/features/projectile/systems/checkCollision.cpp
@@ -4,21 +4,50 @@
 #include "features/enemy/components/aiControlled.hpp"
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <vector>
 
 namespace features::projectile::systems
 {
     void checkCollision(entt::registry &registry)
     {
-        auto projectileView = registry.view<features::projectile::components::isProjectile>();
+        auto projectileView = registry.view<features::projectile::components::isProjectile, common::components::position>();
         auto enemyView = registry.view<features::enemy::components::aiControlled, common::components::position>();  
 
-        for (auto [projectileEntity, isProjectile] : projectileView.each())
+        std::vector<entt::entity> projectilesToDestroy;
+        std::vector<entt::entity> enemiesToDestroy;
+
+        for (auto [projectileEntity, isProjectile, position] : projectileView.each())
         {
             for (auto [enemyEntity, aiControlled, position] : enemyView.each())
             {
                 auto enemyPos = enemyView.get<common::components::position>(enemyEntity);
+                auto projectilePos = projectileView.get<common::components::position>(projectileEntity);
+
+                float deltaX = projectilePos.x - enemyPos.x;
+                float deltaY = projectilePos.y - enemyPos.y;
+                float distance = std::sqrt(deltaX * deltaX + deltaY * deltaY);
+
+                // TODO not hardcoding these values
+                float projectileRadius = 10.f;
+                float enemyRadius = 50.f;
+
+                if (distance < (projectileRadius + enemyRadius))
+                {
+                    projectilesToDestroy.push_back(projectileEntity);
+                    enemiesToDestroy.push_back(enemyEntity);
+                }                
                 // std::cout << enemyPos.x << " " << enemyPos.y << std::endl;
             }
+        }
+
+        for (auto projectile : projectilesToDestroy)
+        {
+            registry.destroy(projectile);
+        }
+
+        for (auto enemy : enemiesToDestroy)
+        {
+            registry.destroy(enemy);
         }
     }
 }
