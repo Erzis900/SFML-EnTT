3ac1933829c1a597df34257ac883d6de9c765981
Piotr Sochacz <piotr.aueternum@gmail.com>
2025-02-17 01:54:42 +0100

    Cleaner crash fix

--- src/features/animation/systems/updateFrame.cpp
+++ src/features/animation/systems/updateFrame.cpp
@@ -19,10 +19,7 @@ namespace features::animation::systems
 				if (timer.value > animationLoader.getTotalTime() || hitbox.lifeSpan <= 0.f || hitbox.hitCount <= 0.f)
 				{
 					registry.remove<common::components::attach>(entity);
-					if (registry.valid(entity))
-					{
-						registry.emplace_or_replace<common::components::remove>(entity);
-					}
+					registry.emplace_or_replace<common::components::remove>(entity);
 				}
 			}
 		}
